1c96d2ad1dd9ed91b634ef4019a08a54bbbf7a95
ccj1994 <a0changj2@gmail.com>
2020-11-26 16:34:45 +0800

    完成編輯功能

--- upload.php
+++ upload.php
@@ -39,82 +39,49 @@ include_once "base.php";
         ];
         print_r($row);
         save("upload",$row);
+        to("manage.php");
     }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>檔案上傳</title>
-    <link rel="stylesheet" href="style.css">
-    <style>
-        table{
-            border:1px solid grey;
-            padding:20px;
-            border-collapse:collapse;
-        }
-        td{
-            border:1px solid #eee;
-            padding:5px;
-        }
-    </style>
-</head>
-<body>
-<h1 class="header">檔案上傳練習</h1>
-<!----建立你的表單及設定編碼----->
-<form action="?" method="post" enctype="multipart/form-data">
-    <div>上傳的檔案:<input type="file" name="img"></div>
-    <div>檔案說明:<input type="text" name="note"></div>
-    <div>檔案類型:
-        <select name="type">
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>檔案上傳</title>
+        <link rel="stylesheet" href="style.css">
+        <style>
+            form{
+                border:3px solid blue;
+                margin:auto;
+                padding:20px;
+                width:300px;
+            }
+            form div{
+                margin:5px 0;
+            }
+        </style>
+    </head>
+    <body>
+     <h1 class="header">檔案上傳練習</h1>
+     <!----建立你的表單及設定編碼----->
+    <form action="?" method="post" enctype="multipart/form-data">
+        <div>上傳的檔案:<input type="file" name="img"></div>
+        <div>檔案說明<input type="text" name="note"></div>
+        <div>檔案類型<select name="type">
             <option value="圖檔">圖檔</option>
             <option value="文件">文件</option>
-            <option value="其他">其他</option>
-        </select>
-    </div>
-    <input type="submit" value="上傳">
-</form>
-
-
-
-
-<!----建立一個連結來查看上傳後的圖檔---->  
-<?php
-$rows=all('upload');
-
-echo "<table>";
-echo "<td>縮圖</td>";
-echo "<td>檔案名稱</td>";
-echo "<td>檔案類型</td>";
-echo "<td>檔案說明</td>";
-echo "<td>下載</td>";
-
-foreach($rows as $row){
-    echo "<tr>";
-
-    if($row['type']=='圖檔'){
-
-        echo "<td><img src='{$row['path']}' style='width:100px;'></td>";
-    }else{
-        echo "<td><img src='./img/file_icon.png' style='width:100px;'></td>";
-
-    }
-
-    echo "<td>{$row['name']}</td>";
-    echo "<td>{$row['type']}</td>";
-    echo "<td>{$row['note']}</td>";
-    echo "<td><a href='{$row['path']}' download>下載</a></td>";
-
-    echo "</tr>";
-}
-
-
-echo "</td>";
-
-
-?>
-
-</body>
-</html>
+            <option value="其它">其它</option>
+        </select></div>
+        <input type="submit" value="上傳">
+    </form>
+    
+    
+    
+    
+    <!----建立一個連結來查看上傳後的圖檔---->  
+    
+    
+    </body>
+    </html>
