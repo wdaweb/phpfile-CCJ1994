9595d0fd85cbf8385a95fdf1f456e559a3a870e8
ccj1994 <a0changj2@gmail.com>
2020-11-30 10:42:14 +0800

    匯出資料表的前置作業,在畫面上顯示要匯出的資料內容

--- text-import.php
+++ text-import.php
@@ -14,16 +14,24 @@
         move_uploaded_file($_FILES['txt']['tmp_name'],"./uplode/".$_FILES['txt']['name']);
 
         $file=fopen("./uplode/".$_FILES['txt']['name'],"r");
-        $line=fgets($file);
-        echo $line;
-        $line=explode(",",$line);
-        $data=[
-            'name'=>$line[1],
-            'age'=>$line[2],
-            'birthday'=>$line[3],
-            'addr'=>$line[4]
-        ];
-        save('students',$data);
+
+        $num=0;
+        while(!feof($file)){
+            $line=fgets($file);
+            if($num!=0){
+                $line=explode(",",$line);
+                $data=[
+                    'name'=>$line['1'],
+                    'age'=>$line['2'],
+                    'birthday'=>$line['3'],
+                    'addr'=>$line['4'],
+                ];
+                save('students',$data);
+            }
+            $num++;
+        }
+        fclose($file);
+        
 
     }
 
